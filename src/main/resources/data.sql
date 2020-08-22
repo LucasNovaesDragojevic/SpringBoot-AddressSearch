96e771aa29fd3ffebe02c290e216e64c00bdc724
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-08-22 20:08:20 -0300

    Complete config files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ADDRESS (street_name, number, complement, neighbourhood, city, state, country, zipcode, latitude, longitude) 
+	VALUES('rua azul', '120', 'casa 2', 'vila cores', 'são paulo', 'são paulo', 'brasil', '00000-000', '000.789', '001.789');
+
+INSERT INTO ADDRESS (street_name, number, complement, neighbourhood, city, state, country, zipcode, latitude, longitude) 
+	VALUES('rua vermelha', '63', '', 'vila cores', 'são paulo', 'são paulo', 'brasil', '00000-000', '000.556', '001.258');
+
+INSERT INTO ADDRESS (street_name, number, complement, neighbourhood, city, state, country, zipcode, latitude, longitude) 
+	VALUES('rua verde', '90', 'apartamento 1', 'vila cores', 'são paulo', 'são paulo', 'brasil', '00000-000', '033.002', '008.556');
