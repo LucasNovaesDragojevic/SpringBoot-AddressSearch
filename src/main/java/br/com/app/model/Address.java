5a7113800d7f482025f6c79ab6ba6506aac509e7
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-08-22 20:06:59 -0300

    Adjust strategy of model

--- src/main/java/br/com/app/model/Address.java
+++ src/main/java/br/com/app/model/Address.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -13,7 +14,7 @@ import lombok.Setter;
 @Getter @Setter
 public class Address 
 {
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String streetName;
 	private String number;
